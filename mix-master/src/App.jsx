4a8b2340225ed44cfc51e176dbe6c3bff1124210
Panji <panji9502@gmail.com>
2023-06-22 20:48:40 +0700

    add components

--- mix-master/src/App.jsx
+++ mix-master/src/App.jsx
@@ -1,4 +1,49 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  HomeLayout,
+  Landing,
+  Error,
+  NewsLetter,
+  Cocktail,
+  SinglePageError,
+} from "./pages";
+
+import { loader as landingLoader } from "./pages/Landing";
+import { loader as SingleCocktailLoader } from "./pages/Cocktail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <SinglePageError />,
+        loader: landingLoader,
+      },
+      {
+        path: "cocktail/:id",
+        errorElement: <SinglePageError />,
+        loader: SingleCocktailLoader,
+        element: <Cocktail />,
+      },
+      {
+        path: "newsLetter",
+        element: <NewsLetter />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <RouterProvider router={router} />;
 };
 export default App;
---
+++ mix-master/src/pages/NewsLetter.jsx
@@ -0,0 +1,5 @@
+const NewsLetter = () => {
+  return <h2>NewsLetter</h2>;
+};
+
+export default NewsLetter;

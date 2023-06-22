cd5c748c5567f0a5e727698bc6f95904392fbbe7
Panji <panji9502@gmail.com>
2023-06-22 21:11:15 +0700

    add newsletter form

--- mix-master/src/pages/NewsLetter.jsx
+++ mix-master/src/pages/NewsLetter.jsx
@@ -1,5 +1,57 @@
 const NewsLetter = () => {
-  return <h2>NewsLetter</h2>;
+  return (
+    <form className="form" method="POST">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        our newsLetter
+      </h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue="jhon"
+        />
+      </div>
+      {/* lastName */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          defaultValue="smith"
+        />
+      </div>
+      {/* email */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="email "
+          id="email"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: "0.5rem" }}
+      >
+        submit
+      </button>
+    </form>
+  );
 };
 
 export default NewsLetter;

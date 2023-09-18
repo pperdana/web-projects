c71dceb540fd99eb8ae53e79a3d374bfc66c99b2
Panji <panji9502@gmail.com>
2023-09-18 18:07:55 +0700

    add starter

---
+++ unsplash-images/starter/src/App.jsx
@@ -0,0 +1,12 @@
+import Gallery from "./Gallery";
+import SearchForm from "./SearchForm";
+
+const App = () => {
+  return (
+    <main>
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;

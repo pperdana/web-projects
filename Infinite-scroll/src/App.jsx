10eb6c43611a6096f73c357069fc8db0397e07ab
Panji <panji9502@gmail.com>
2023-09-23 23:04:53 +0700

    update

--- Infinite-scroll/src/App.jsx
+++ Infinite-scroll/src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [photosArray, setPhotosArray] = useState([]);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const [totalImages, setTotalImages] = useState(0);
+  const [ready, setReady] = useState(false);
+
+  const count = 10;
+  const apiKey = "Qm5r7U4Lck1w1Y1SW2AK6nNKmxsXjwrAphEixhu48is";
+  const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+  // Check if all images were loaded
+  function imageLoaded() {
+    setImagesLoaded((prevImagesLoaded) => prevImagesLoaded + 1);
+  }
+
+  // Helper Function to Set Attributes on JSX Elements
+  function createImageJSX(photo) {
+    return (
+      <a key={photo.id} href={photo.links.html} target="blank">
+        <img
+          src={photo.urls.regular}
+          alt={photo.alt_description}
+          title={photo.alt_description}
+          onLoad={imageLoaded}
+          className="img"
+        />
+      </a>
+    );
+  }
+
+  // Get photos from Unsplash API
+  async function getPhotos() {
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setPhotosArray((prevPhotosArray) => [...prevPhotosArray, ...data]);
+    } catch (error) {
+      // Catch Error Here
+    }
+  }
+
+  // Check to see if scrolling near the bottom of the page, Load More Photos
+  function handleScroll() {
+    if (
+      window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight - 1000 &&
+      ready
+    ) {
+      setReady(false);
+      getPhotos();
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    // On Load
+    getPhotos();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [ready]);
+
+  useEffect(() => {
+    if (imagesLoaded === totalImages) {
+      setReady(true);
+    }
+  }, [imagesLoaded, totalImages]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div id="loader">{!ready ? "Loading..." : null}</div>
+      <div className="gallery">
+        {photosArray.map((photo) => createImageJSX(photo))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;

019fa7b38e6b5a470c436c03ca10792ddf7be245
Panji <panji9502@gmail.com>
2023-10-21 17:30:23 +0700

    style app with tailwind

--- birthday-tstw/src/App.tsx
+++ birthday-tstw/src/App.tsx
@@ -5,13 +5,15 @@ import List from "./List";
 function App() {
   const [people, setPeople] = useState(data);
   return (
-    <main>
-      <section className="container">
-        <h3>{people.length} birthdays today</h3>
+    <main className="min-h-screen	flex justify-center	items-center">
+      <section className="container w-[90vw] max-w-[600px] my-[5rem] mx-0 bg-white	rounded-[0.25rem] py-[1.5rem] px-[2rem] shadow-xl">
+        <h3 className="mb-[2rem] text-[1.953rem]">
+          {people.length} birthdays today
+        </h3>
         <List people={people} />
         <button
           type="button"
-          className="btn btn-block"
+          className="btn btn-block w-full cursor-pointer	text-white bg-[#d946ef] border-transparent rounded-[0.25rem] tracking-[1px] py-[0.375rem] px-[0.75rem] shadow-xl transition-all duration-300	ease-in-out capitalize inline-block	hover:bg-[#a21caf] hover:shadow-2xl"
           onClick={() => setPeople([])}
         >
           clear all
--- birthday-tstw/tailwind.config.js
+++ birthday-tstw/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        xl: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+        "2xl":
+          "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+      },
+    },
   },
   plugins: [],
 };

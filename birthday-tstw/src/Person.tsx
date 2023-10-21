3fe0bd4e080afbae941fe70be67ac622bbc35ccf
Panji <panji9502@gmail.com>
2023-10-21 20:03:10 +0700

    style with tailwind

--- birthday-tstw/src/Person.tsx
+++ birthday-tstw/src/Person.tsx
@@ -1,9 +1,13 @@
 const Person = ({ image, name, age }) => {
   return (
-    <article className='person'>
-      <img src={image} alt={name} className='img' />
+    <article className="person grid grid-cols-[auto_1fr] gap-[0.75rem] mb-[1.5rem] items-center">
+      <img
+        src={image}
+        alt={name}
+        className="img w-[75px] h-[75px] rounded-[50%] shadow-2xl block object-cover"
+      />
       <div>
-        <h4>{name}</h4>
+        <h4 className="font-medium	mb-[0.5rem] text-[1.563rem]">{name}</h4>
         <p>{age} years</p>
       </div>
     </article>

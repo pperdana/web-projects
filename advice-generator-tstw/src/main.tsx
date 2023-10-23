a9157c17dcf2674fc9234fe78077c7cbb11fa709
Panji <panji9502@gmail.com>
2023-10-23 16:52:53 +0700

    add typescript

--- advice-generator-tstw/src/main.tsx
+++ advice-generator-tstw/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./index.css";
-
 import { AppProvider } from "./context";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </React.StrictMode>
+  );
+}

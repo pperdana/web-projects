725d6a1df9bf490e8b9a67b1dbf0c871375b0e46
Panji <panji9502@gmail.com>
2023-11-16 15:38:16 +0700

    update

--- mix-master/src/App.jsx
+++ mix-master/src/App.jsx
@@ -59,7 +59,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 };

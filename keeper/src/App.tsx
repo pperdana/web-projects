5e066c1b6083d64479c8a60838cc0ee6b4bedf9c
Panji <panji9502@gmail.com>
2023-11-18 21:56:47 +0700

    update app

--- keeper/src/App.tsx
+++ keeper/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import NoteList from "./components/NoteList";
+import Header from "./components/Header";
+import CreateArea from "./components/CreateArea";
+import Footer from "./components/Footer";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Note {
+  id: number;
+  content: string;
 }
 
-export default App
+const App: React.FC = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const addNote = (newNote: Note) => {
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
+
+  const deleteNote = (id: number) => {
+    setNotes((prevNotes) => prevNotes.filter((_, index) => index !== id));
+  };
+
+  return (
+    <div className="">
+      <Header />
+      <CreateArea onAdd={addNote} />
+      <NoteList notes={notes} deleteNote={deleteNote} />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;

e01db62d07f1ed5e70f728c8ee35320e49c7c376
Panji <panji9502@gmail.com>
2023-11-28 11:03:37 +0700

    define note hight

--- keeper/src/components/Note.tsx
+++ keeper/src/components/Note.tsx
@@ -14,9 +14,11 @@ const Note: React.FC<NoteProps> = ({ id, title, content, onDelete }) => {
   };
 
   return (
-    <div className="note">
+    <div className="note h-fit">
       <h1 className="text-[1.1em] mb-[6px] font-semibold">{title}</h1>
-      <p className="text-[1.1em] mb-[10px] whitespace-pre-wrap">{content}</p>
+      <p className="text-[1.1em] mb-[10px] whitespace-pre-wrap pb-[10px]">
+        {content}
+      </p>
       <button onClick={handleClick}>
         <DeleteIcon />
       </button>
